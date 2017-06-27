d56508d273f2bc40b49ca3803cbc0fba983fa3c0
Benjamin <bennievv@live.nl>
2017-06-27 13:50:24 +0200

    Commentaar + punten tellen

--- Nakijktool Joris/NakijkTool/ExcelWriter.cs
+++ Nakijktool Joris/NakijkTool/ExcelWriter.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace NakijkTool
 {
@@ -20,12 +21,14 @@ namespace NakijkTool
                 DataSet vragen = new DataSet();
                 DataSet students = new DataSet();
                 DataSet commentaar = new DataSet();
+                DataSet opdr = new DataSet();
                 decimal totaalpunten = 0;
                 int totaalvragen = 0;
                 int totaalstudenten = 0;
                 string queryTest = $"SELECT * FROM Tentamens WHERE tentamenid = {tentamenID}";
-                string queryStudents = $"SELECT DISTINCT studentnummer, student_naam FROM Testrapport WHERE tentamenid = {tentamenID}";
-                string queryVragen = $"SELECT * FROM Vraag WHERE tentamenid = {tentamenID} ORDER BY vraagnummer";
+                string queryStudents = $"SELECT distinct studentnummer, student_naam FROM Testrapport WHERE tentamenid = {tentamenID}";
+                string queryOpdrachten = "SELECT * FROM Testrapport WHERE tentamenid = @tentamenid";
+                string queryVragen = $"SELECT * FROM Vraag WHERE tentamenid = {tentamenID} ORDER BY vraagnummer ASC";
                 string queryCommentaar = "SELECT * FROM Commentaar WHERE vraagid in (SELECT DISTINCT vraagid FROM Testrapport WHERE tentamenid = @tentamenid)";
                 using (SqlDataAdapter command = new SqlDataAdapter(queryTest, connection))
                 {
@@ -47,12 +50,16 @@ namespace NakijkTool
                     command.SelectCommand.Parameters.AddWithValue("tentamenid", tentamenID);
                     command.Fill(commentaar);
                 }
+                using (SqlDataAdapter command = new SqlDataAdapter(queryOpdrachten, connection))
+                {
+                    command.SelectCommand.Parameters.AddWithValue("tentamenid", tentamenID);
+                    command.Fill(opdr);
+                }
                 Microsoft.Office.Interop.Excel.Application oXL;
                 Microsoft.Office.Interop.Excel.Workbook oWB;
                 Microsoft.Office.Interop.Excel.Worksheet oSheet;
                 object misvalue = System.Reflection.Missing.Value;
                 oXL = new Microsoft.Office.Interop.Excel.Application();
-                oXL.Visible = true;
                 oWB = (Microsoft.Office.Interop.Excel.Workbook)(oXL.Workbooks.Add(""));
                 oSheet = oWB.Worksheets[1];
                 oSheet.Name = "Toetsresultaten";
@@ -100,56 +107,50 @@ namespace NakijkTool
                     oSheet.Cells[c + m, "A"] = studentnaam;
                     oSheet.Cells[c + m, "B"] = studentnr;
                     // Opdrachten
-                    DataSet opdr = new DataSet();
-                    string queryOpdrachten = $"SELECT * FROM Testrapport WHERE tentamenid = @tentamenid AND studentnummer = @studentnr ORDER BY vraagnummer";
-                    using (SqlDataAdapter command = new SqlDataAdapter(queryOpdrachten, connection))
+                    DataRow[] o = opdr.Tables[0].Select($"studentnummer = '{studentnr}'", "studentnummer ASC");
+                    for (int v = 0; v < totaalvragen; v++)
                     {
-                        command.SelectCommand.Parameters.AddWithValue("tentamenid", tentamenID);
-                        command.SelectCommand.Parameters.AddWithValue("studentnr", studentnr);
-                        command.Fill(opdr);
-                        for (int v = 0; v < totaalvragen; v++)
+                        punten += Convert.ToInt16(o[v]["studentpunten"]);
+                        Microsoft.Office.Interop.Excel.Worksheet newSheet;
+                        newSheet = oWB.Worksheets[v + 2];
+                        newSheet.Cells[c, "A"] = studentnaam;
+                        newSheet.Cells[c, "B"] = studentnr;
+                        newSheet.Cells[c, "D"] = o[v]["studentpunten"];
+                        newSheet.Cells[c, "E"] = o[v]["commentaartext"];
+                        newSheet.Range[$"A{c}"].EntireRow.EntireColumn.AutoFit();
+                        string error = o[v]["errors"].ToString();
+                        if (error.StartsWith("Correct"))
                         {
-                            punten += Convert.ToInt16(opdr.Tables[0].Rows[v]["studentpunten"]);
-                            Microsoft.Office.Interop.Excel.Worksheet newSheet;
-                            newSheet = oWB.Worksheets[v + 2];
-                            newSheet.Cells[c, "A"] = studentnaam;
-                            newSheet.Cells[c, "B"] = studentnr;
-                            newSheet.Cells[c, "D"] = opdr.Tables[0].Rows[v]["studentpunten"];
-                            newSheet.Cells[c, "E"] = opdr.Tables[0].Rows[v]["commentaartext"];
-                            newSheet.Range[$"A{c}"].EntireRow.EntireColumn.AutoFit();
-                            string error = opdr.Tables[0].Rows[v]["errors"].ToString();
-                            if (error.StartsWith("Correct"))
-                            {
-                                ac++;
-                                newSheet.Cells[c, "C"] = "Correct!";
-                                newSheet.Range[$"C{c}"].Interior.ColorIndex = 43;
-                            }
-                            else if (error.StartsWith("ExceptionDuringExecution"))
-                            {
-                                er++;
-                                newSheet.Cells[c, "C"] = "Runtime error";
-                                newSheet.Range[$"C{c}"].Interior.ColorIndex = 44;
-                            }
-                            else if (error.StartsWith("CompileError"))
-                            {
-                                ec++;
-                                newSheet.Cells[c, "C"] = "Compile error";
-                                newSheet.Range[$"C{c}"].Interior.ColorIndex = 46;
-                            }
-                            else if (error.StartsWith("Open vraag"))
-                            {
-                                newSheet.Cells[c, "C"] = "Open vraag";
-                                newSheet.Range[$"C{c}"].Interior.ColorIndex = 15;
-                            }
-                            else
-                            {
-                                unknown = true;
-                                newSheet.Cells[c, "C"] = "Anders...";
-                                newSheet.Range[$"C{c}"].Interior.ColorIndex = 48;
-                            }
+                            ac++;
+                            newSheet.Cells[c, "C"] = "Correct!";
+                            newSheet.Range[$"C{c}"].Interior.ColorIndex = 43;
+                        }
+                        else if (error.StartsWith("ExceptionDuringExecution"))
+                        {
+                            er++;
+                            newSheet.Cells[c, "C"] = "Runtime error";
+                            newSheet.Range[$"C{c}"].Interior.ColorIndex = 44;
+                        }
+                        else if (error.StartsWith("CompileError"))
+                        {
+                            ec++;
+                            newSheet.Cells[c, "C"] = "Compile error";
+                            newSheet.Range[$"C{c}"].Interior.ColorIndex = 46;
+                        }
+                        else if (error.StartsWith("Open vraag"))
+                        {
+                            newSheet.Cells[c, "C"] = "Open vraag";
+                            newSheet.Range[$"C{c}"].Interior.ColorIndex = 15;
+                        }
+                        else
+                        {
+                            unknown = true;
+                            newSheet.Cells[c, "C"] = "Anders...";
+                            newSheet.Range[$"C{c}"].Interior.ColorIndex = 48;
                         }
                     }
-                    decimal cijfer = punten / totaalpunten;
+                    decimal cijfer = Math.Round(((punten / totaalpunten) * 9) + 1, 1);
+                    if (cijfer > 10.0m) cijfer = 10.0m;
                     oSheet.Cells[c + m, "C"] = cijfer;
                     if (er == 0 && ec == 0)
                     {
@@ -178,32 +179,32 @@ namespace NakijkTool
                     }
                     oSheet.Range[$"A{c}"].EntireRow.EntireColumn.AutoFit();
                 }
-
-                // tijdelijk uit omdat het de excelwriter crashed
-                if (false) {
-                    for (int i = 0; i < totaalvragen; i++)
+                for (int i = 0; i < totaalvragen; i++)
+                {
+                    Microsoft.Office.Interop.Excel.Worksheet commentSheet;
+                    commentSheet = oWB.Worksheets[i + 2];
+                    int x = totaalstudenten + 5;
+                    DataRow r = vragen.Tables[0].Rows[i];
+                    DataRow[] q = commentaar.Tables[0].Select($"vraagid = {r["vraagid"]}");
+                    DataRow[] t = opdr.Tables[0].Select($"vraagid = {r["vraagid"]}");
+                    int[] count = new int[q.Length];
+                    for (int j = 0; j < q.Length; j++)
                     {
-                        Microsoft.Office.Interop.Excel.Worksheet commentSheet;
-                        commentSheet = oWB.Worksheets[i + 1];
-                        int x = totaalvragen + 5;
-                        DataRow r = vragen.Tables[0].Rows[i];
-                        DataRow[] q = commentaar.Tables[0].Select($"vraagid = {r["vraagid"]}");
-                        DataRow[] t = test.Tables[0].Select($"vraagid = {r["vraagid"]}");
-                        int[] count = new int[q.Length];
-                        for (int j = 0; j < q.Length; j++)
+                        foreach (DataRow ts in t)
                         {
-                            foreach (DataRow ts in t)
-                            {
+                            //todo: werkt nog niet helemaal goed
+                            if (ts["commentaar"].ToString().Length != 0 && ts["commentaar"].ToString().Length >= j)
                                 if (ts["commentaar"].ToString()[j] == '1')
                                 {
                                     count[j]++;
                                 }
-                            }
-                            commentSheet.Cells[x, "A"] = q[j]["commentaarnaam"];
-                            commentSheet.Cells[x, "B"] = count[j];
                         }
+                        commentSheet.Cells[x, "A"] = q[j]["commentaarnaam"];
+                        commentSheet.Cells[x, "B"] = count[j];
+                        x++;
                     }
                 }
+                oXL.Visible = true;
             }
         }
         /// <summary>
